59054df2a4048422b13a24e6186f0ae71492471a
Esmaeill <liyanboy74@gmail.com>
2022-07-22 13:01:44 +0430

    create cocket

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main(int argc , char *argv[])
 {
 	WSADATA wsa;
+    SOCKET s;
 
 	printf("\nInitialising Winsock...");
 	if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
@@ -12,7 +13,14 @@ int main(int argc , char *argv[])
 		return 1;
 	}
 
-	printf("Initialised.");
+	printf("Initialised.\n");
+
+    if((s=socket(AF_INET,SOCK_STREAM,0))==INVALID_SOCKET)
+    {
+        printf("Could not create socket : %d",WSAGetLastError());
+    }
+
+    printf("Socket created.\n");
 
 	return 0;
 }
